2a902ff9e9e1b55445cc7eb3620f8b7cf530e63c
mps-laptop <ups1675@gmail.com>
2020-01-18 10:59:35 -0600

    Added Caeleb's suggested changes in formatting and naming

--- src/main/java/frc/robot/subsystems/ColorWheel.java
+++ src/main/java/frc/robot/subsystems/ColorWheel.java
@@ -28,26 +28,25 @@ public class ColorWheel extends SubsystemBase {
 
   private final I2C.Port i2cPort = I2C.Port.kOnboard;
 
-  private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
+  private final ColorSensorV3 colorSensor = new ColorSensorV3(i2cPort);
 
-  private final ColorMatch m_colorMatcher = new ColorMatch();
+  private final ColorMatch colorMatcher = new ColorMatch();
 
   private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
   private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
   private final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
   private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
   private String currentColor;
+
   public ColorWheel() {
-
     spinMotor = new VictorSPX(7);
-
   }
 
   public void robotInit() {
-    m_colorMatcher.addColorMatch(kBlueTarget);
-    m_colorMatcher.addColorMatch(kGreenTarget);
-    m_colorMatcher.addColorMatch(kRedTarget);
-    m_colorMatcher.addColorMatch(kYellowTarget); 
+    colorMatcher.addColorMatch(kBlueTarget);
+    colorMatcher.addColorMatch(kGreenTarget);
+    colorMatcher.addColorMatch(kRedTarget);
+    colorMatcher.addColorMatch(kYellowTarget);
   }
 
   public void turnWheel() {
@@ -69,13 +68,13 @@ public class ColorWheel extends SubsystemBase {
      * raw color, use GetRawColor().
      * 
      */
-    Color detectedColor = m_colorSensor.getColor();
+    Color detectedColor = colorSensor.getColor();
 
     /**
      * Run the color match algorithm on our detected color
      */
     String colorString;
-    ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
+    ColorMatchResult match = colorMatcher.matchClosestColor(detectedColor);
 
     if (match.color == kBlueTarget) {
       colorString = "Blue";
