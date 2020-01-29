f35f54d61c2111302a1910c5f1ed68d8bad265f6
Lanning <lanninjd@milwaukee.k12.wi.us>
2020-01-28 19:02:44 -0600

    Cleaned up the turn to angle code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -37,8 +37,8 @@ public class RobotContainer {
   private final JoystickButton operatorControllerRightBumper = new JoystickButton(operatorController,
       Constants.RIGHT_BUMPER);
 
-  // private Drive2019 drive = new Drive2019(); (Disables the 2019 drive, suggested by Justin because we changed the wheel motor from 4 to 1)
-  private ColorWheel colorWheel = new ColorWheel();
+  private Drive2019 drive = new Drive2019(); //(Disables the 2019 drive, suggested by Justin because we changed the wheel motor from 4 to 1)
+  //private ColorWheel colorWheel = new ColorWheel();
   // private ColorWheel colorWheel = new ColorWheel();
   // private Arm arm = new Arm();
 
@@ -102,8 +102,8 @@ public class RobotContainer {
    * passing it to a {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
-    // drive.setDefaultCommand(new CheesyDrive(drive, () -> getDriverLeftYAxis(), () -> getDriverRightXAxis())); (Commented out with line 36 for 2019 drive)
-    colorWheel.setDefaultCommand(new RotationControl(colorWheel, Constants.ROTATION_COUNTS_NEEDED));
+    drive.setDefaultCommand(new CheesyDrive(drive, () -> getDriverLeftYAxis(), () -> getDriverRightXAxis())); //(Commented out with line 36 for 2019 drive)
+    //colorWheel.setDefaultCommand(new RotationControl(colorWheel, Constants.ROTATION_COUNTS_NEEDED));
     // operatorControllerLeftBumper.whileHeld(new ReverseWheel());
     // operatorControllerRightBumper.whileHeld(new SpinWheel());
     //arm.setDefaultCommand(new MoveArm(arm, () -> getOperatorLeftYAxis()));
@@ -116,9 +116,9 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return null;
+    //return null;
     // return new DriveToDistance(drive).beforeStarting(() -> drive.resetPosition());
     // commenting out one of the auto commands for now -Caeleb
-    // return new TurnToAngle(drive, 90);
+    return new TurnToAngle(drive, 90);
   }
 }
