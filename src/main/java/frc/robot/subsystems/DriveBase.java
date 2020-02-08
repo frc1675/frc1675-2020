40fddfe18487df8d4dd78051a07527acb578d1ad
Lanning <lanninjd@milwaukee.k12.wi.us>
2020-02-08 11:55:42 -0600

    Finally fixed the Encoder

--- src/main/java/frc/robot/subsystems/DriveBase.java
+++ src/main/java/frc/robot/subsystems/DriveBase.java
@@ -23,11 +23,9 @@ public class DriveBase extends SubsystemBase {
   private CANSparkMax leftBack;
   private CANSparkMax rightFront;
   private CANSparkMax rightBack;
-  CANEncoder leftAlternateEncoder;
-  CANEncoder rightAlternateEncoder;
-  // private CANSparkMax leftMiddle;
-  // private CANSparkMax rightMiddle;
-  public AHRS navx;
+  private CANEncoder leftAlternateEncoder;
+  private CANEncoder rightAlternateEncoder;
+  private AHRS navx;
   private static final AlternateEncoderType kAltEncType = AlternateEncoderType.kQuadrature;
   private static final int kCPR = 8192;
 
@@ -35,54 +33,47 @@ public class DriveBase extends SubsystemBase {
    * Creates a new Drive.
    */
   public DriveBase() {
-    // rightMiddle = new CANSparkMax(Constants.RIGHT_MIDDLE, MotorType.kBrushless);
-    // leftMiddle = new CANSparkMax(Constants.LEFT_MIDDLE, MotorType.kBrushless);
     rightBack = new CANSparkMax(Constants.RIGHT_BACK, MotorType.kBrushless);
     rightFront = new CANSparkMax(Constants.RIGHT_FRONT, MotorType.kBrushless);
     leftBack = new CANSparkMax(Constants.LEFT_BACK, MotorType.kBrushless);
     leftFront = new CANSparkMax(Constants.LEFT_FRONT, MotorType.kBrushless);
     navx = new AHRS(SerialPort.Port.kMXP);
 
-    leftAlternateEncoder = leftFront.getAlternateEncoder(kAltEncType, kCPR);
-    rightAlternateEncoder = rightFront.getAlternateEncoder(kAltEncType, kCPR);
-
+    leftAlternateEncoder = leftBack.getAlternateEncoder(kAltEncType, kCPR);
+    rightAlternateEncoder = rightBack.getAlternateEncoder(kAltEncType, kCPR);
+    rightAlternateEncoder.setInverted(true);
+    leftAlternateEncoder.setPosition(0);
+    rightAlternateEncoder.setPosition(0);
   }
 
   public void setRightMotors(double power) {
     rightFront.set(power);
-    // rightMiddle.set(power);
     rightBack.set(power);
   }
 
   public void setLeftMotors(double power) {
     leftFront.set(-power);
-    // leftMiddle.set(-power);
     leftBack.set(-power);
   }
 
-  // public int getPosition(){
-  // rightMiddle.getEncoder();
-  // int rightPosition = rightMiddle.getPosition();
-  // int leftPosition = leftMiddle.getSelectedSensorPosition();
-  // int averagePosition = (rightPosition + leftPosition)/2;
+   public double getPosition(){
+   double leftEncoderValue = leftAlternateEncoder.getPosition();
+   double rightEncoderValue = rightAlternateEncoder.getPosition();
+   double averagePosition = (rightEncoderValue + leftEncoderValue)/2;
+   SmartDashboard.putNumber("LeftEncoder", leftEncoderValue);
+   SmartDashboard.putNumber("RightEncoder", rightEncoderValue);
 
-  // //currentPosition = currentPosition + 1;
-  // //return currentPosition;
-  // return averagePosition;
-  // }
+   return averagePosition;
+   }
 
-  // public void resetPosition(){
-  // rightMiddle.setSelectedSensorPosition(0);
-  // leftMiddle.setSelectedSensorPosition(0);
-  // currentPosition = 0;
-  // }
+   public void resetPosition(){
+    leftAlternateEncoder.setPosition(0);
+    rightAlternateEncoder.setPosition(0);
+   }
 
   @Override
   public void periodic() {
-    double leftEncoderValue = leftAlternateEncoder.getPosition();
-    double rightEncoderValue = rightAlternateEncoder.getPosition();
-    System.out.println("left: " + leftEncoderValue);
-    System.out.println("right: " + rightEncoderValue);
+    SmartDashboard.putNumber("Position", getPosition());
     SmartDashboard.putNumber("Angle", getAngle());
   }
 
@@ -97,6 +88,4 @@ public class DriveBase extends SubsystemBase {
     return heading;
   }
 
-  public void robotInit() {
-  }
 }
